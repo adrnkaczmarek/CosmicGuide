f0ce0923d56ad5740cbf6d7d0b23c89e0601c4d3
Adrian <adrian_kaczmarek94@wp.pl>
2016-02-24 22:42:17 +0100

    Added viewpager

--- app/src/main/java/com/example/adrian/cosmicguide/MainActivity.java
+++ app/src/main/java/com/example/adrian/cosmicguide/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.adrian.cosmicguide;
 
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.adrian.cosmicguide.Earth.EarthFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CollectionPagerAdapter adapter = new CollectionPagerAdapter(getSupportFragmentManager());
+        pager = (ViewPager) findViewById(R.id.mainSwiper);
+        pager.setAdapter( adapter );
     }
 
     @Override
@@ -39,5 +48,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class CollectionPagerAdapter extends FragmentPagerAdapter{
+
+        public CollectionPagerAdapter(FragmentManager ft){
+            super(ft);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = new EarthFragment();
+            Bundle args = new Bundle();
+            args.putInt("object", position + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
